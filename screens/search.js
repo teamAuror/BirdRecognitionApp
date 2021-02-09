c46fd04f09285836afa88592d75255d3dd8ef16f
coderx31 <shenal.fernando10@gmail.com>
2021-02-09 15:53:28 +0530

    add some styles to search screen

--- screens/search.js
+++ screens/search.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import { StyleSheet, View, Text, ImageView } from 'react-native';
+import { StyleSheet, View, Text, ImageBackground } from 'react-native';
 
 export default function Search(){
     return(
-        <View>
-            <Text>Search Screen</Text>
-        </View>
+        <ImageBackground source={require('../assets/images/search_bg.jpg')} style={styles.bgImage}>
+            <View>
+                <Text>Search Screen</Text>
+            </View>
+        </ImageBackground>
+        
     );
 }
+
+const styles = StyleSheet.create({
+    bgImage: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        resizeMode: 'cover',
+        width: '100%',
+        height: '100%',
+    },
+})
