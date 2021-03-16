2c96e6d3fe8db5be5961a8957fb594f3b256b3e0
unknown <vajithc@gmail.com>
2021-03-16 13:52:51 +0530

    Modified the help screen

--- clientside/screens/helpCenter.js
+++ clientside/screens/helpCenter.js
@@ -6,46 +6,62 @@ export default function helpCenter(){
     return(
         
         <ImageBackground source={require('../assets/images/about_bg.png')} style={globalStyles.bgImageContainer}>
-            
             <View>
-                <h2>General Questions</h2>
+            <Text style={styles.qTypeTxt}>General Questions</Text>
+                <Text style={styles.quesTxt}>
+                    What is BirdO ?
+                </Text>
                 <Text style={styles.helpTxt}>
-                    <b>What is BirdO ?</b>
                     BirdO is the number 1 app for bird identification. It can identify birds by their snaps. 
                     it taps into hundreds of records within our database to find out what bird it is that you 
                     most likely encountered in the vicinity of where you saw the bird. 
                 </Text>
+                <Text style={styles.quesTxt}>
+                    How does BirdO works ?
+                </Text>
                 <Text style={styles.helpTxt}>
-                    <b>How does BirdO works ?</b>
                     BirdO will provide you with factual information about the bird you just encountered which 
                     are collected from reliable and recognized information points on the internet.
                 </Text>
+                <Text style={styles.quesTxt}>
+                    How do i contact BirdO ?
+                </Text>
                 <Text style={styles.helpTxt}>
-                    <b>How do i contact BirdO ?</b>
                     You can contact us by,
-                    <Button onPress={()=>Linking.openURL('mailto:support@birdo.net?subject=SendMail&body=Descreption')}
-                     title="support@birdo.net"/>
+                    {/*<Button onPress={()=>Linking.openURL('mailto:support@birdo.net?subject=SendMail&body=Descreption')}
+                     title="support@birdo.net"/>*/}
                     
                 </Text>
+                <Text style={styles.quesTxt}>
+                    Do i need an internet connection to use BirdO ?
+                </Text>
                 <Text style={styles.helpTxt}>
-                    <b>Do i need an internet connection to use BirdO ?</b>
                     Yes you have to have a working internet connection to download and use the BirdO.
                 </Text>
-                <h2>Technical Questions</h2>
+                <Text style={styles.qTypeTxt}>Technical Questions</Text>
+                <Text style={styles.quesTxt}>
+                    What are  the system requirements to run the BirdO app ?
+                </Text>
                 <Text style={styles.helpTxt}>
-                    <b>What are  the system requirements to run the BirdO app ?</b>
                     ios:
                     Android:
                 </Text>
+                <Text style={styles.quesTxt}>
+                    Camera won't focus / stay blurry ?
+                </Text>
                 <Text style={styles.helpTxt}>
-                    <b>Camera won't focus / stay blurry</b>
                     Some devices may have problems focusing on near objects with shorter distances. 
                     Try to hold your device with some distance then you can get more results. 
                 </Text>
+                <Text style={styles.quesTxt}>
+                    Camera screen stays black 
+                </Text>
                 <Text style={styles.helpTxt}>
-                    <b>Camera screen stays black</b>
                     Restart your device.
                     Remove third party camera apps.
+                </Text>
+                <Text style={styles.quesTxt}>
+
                 </Text>
                 <Text style={styles.helpTxt}>
                     
@@ -78,6 +94,17 @@ const styles = StyleSheet.create({
       },
       helpTxt:{
         fontSize: 14,
+        fontFamily: 'nunito-regular',
+      },
+      quesTxt:{
+        fontSize: 14,
+        fontWeight:'bold',
+        fontFamily: 'nunito-regular',
+      }
+      ,
+      qTypeTxt:{
+        fontSize: 16,
+        fontWeight:'bold',
         fontFamily: 'nunito-regular',
       }
 });
